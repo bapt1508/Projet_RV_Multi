9e48dbe1c024b32d00d5a8bab0da2e0f7b9617ba
AlexCTZ <alex.caratza@gmail.com>
2025-10-14 14:11:59 +0200

    ligne depart et arrivée sans lien network avec text de fin et countdown

---
+++ Assets/FactoryFrenzy/Scripts/StartLineManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using TMPro;
+using Unity.Netcode;
+using System.Collections;
+
+public class StartLineManager : NetworkBehaviour
+{
+    public TMP_Text countdownText;
+    public int countdownTime = 3;
+
+    private NetworkVariable<bool> raceStarted = new NetworkVariable<bool>(false);
+
+    void Start()
+    {
+        if (IsServer)
+            StartCoroutine(StartCountdown());
+    }
+
+    private IEnumerator StartCountdown()
+    {
+        int currentTime = countdownTime;
+
+        while (currentTime > 0)
+        {
+            UpdateCountdownClientRpc(currentTime);
+            yield return new WaitForSeconds(1f);
+            currentTime--;
+        }
+
+        UpdateCountdownClientRpc(0);
+        raceStarted.Value = true;
+        EnableMovementClientRpc();
+    }
+
+    [ClientRpc]
+    private void UpdateCountdownClientRpc(int timeLeft)
+    {
+        if (countdownText == null) return;
+
+        if (timeLeft > 0)
+            countdownText.text = timeLeft.ToString();
+        else
+            countdownText.text = "GO!";
+
+        countdownText.fontSize = timeLeft == 0 ? 120 : 100;
+        countdownText.color = timeLeft == 0 ? Color.green : Color.white;
+    }
+
+    [ClientRpc]
+    private void EnableMovementClientRpc()
+    {
+        foreach (var player in FindObjectsOfType<StarterAssets.ThirdPersonController>())
+        {
+            player.canMove = true;
+        }
+
+        StartCoroutine(ClearCountdownText());
+    }
+
+    private IEnumerator ClearCountdownText()
+    {
+        yield return new WaitForSeconds(1f);
+        countdownText.text = "";
+    }
+}
