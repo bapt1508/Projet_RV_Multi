e9a7128dce34b276f4644b73f9203905cdc51e57
bapt1508 <baptiste.courtade01@gmail.com>
2025-10-15 08:48:54 +0200

    startline fonctionnel (changer l'orientation du spawn) test 1 joueur - bumper non fonctionel

--- Assets/FactoryFrenzy/Scripts/StartLineManager.cs
+++ Assets/FactoryFrenzy/Scripts/StartLineManager.cs
@@ -7,19 +7,32 @@ public class StartLineManager : NetworkBehaviour
 {
     public TMP_Text countdownText;
     public int countdownTime = 3;
+    public Canvas canva;
 
     private NetworkVariable<bool> raceStarted = new NetworkVariable<bool>(false);
 
-    void Start()
+    public override void OnNetworkSpawn()
     {
-        if (IsServer)
+        if (IsServer) {
+            foreach (var player in FindObjectsOfType<StarterAssets.ThirdPersonController>())
+            {
+                player.canMove = false;
+            }
+            canva.enabled = true;
+            Debug.Log("ca commence");
             StartCoroutine(StartCountdown());
+        }
+
+                
+            
     }
 
     private IEnumerator StartCountdown()
     {
-        int currentTime = countdownTime;
 
+        
+        int currentTime = countdownTime;
+        
         while (currentTime > 0)
         {
             UpdateCountdownClientRpc(currentTime);
@@ -35,6 +48,7 @@ public class StartLineManager : NetworkBehaviour
     [ClientRpc]
     private void UpdateCountdownClientRpc(int timeLeft)
     {
+        Debug.Log("Update");
         if (countdownText == null) return;
 
         if (timeLeft > 0)
@@ -42,7 +56,7 @@ public class StartLineManager : NetworkBehaviour
         else
             countdownText.text = "GO!";
 
-        countdownText.fontSize = timeLeft == 0 ? 120 : 100;
+        countdownText.fontSize = timeLeft == 0 ? 120 : 120;
         countdownText.color = timeLeft == 0 ? Color.green : Color.white;
     }
 
@@ -61,5 +75,6 @@ public class StartLineManager : NetworkBehaviour
     {
         yield return new WaitForSeconds(1f);
         countdownText.text = "";
+        canva.enabled = false;
     }
 }
