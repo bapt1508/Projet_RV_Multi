ec312de6695e2d75d795ddbfcf646545fa0dcd73
AlexCTZ <alex.caratza@gmail.com>
2025-10-17 12:05:08 +0200

    bouton k pour que l'host lance la game

---
+++ Assets/GameStarter.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using Unity.Netcode;
+using TMPro;
+
+public class GameStarter : NetworkBehaviour
+{
+    [SerializeField] private TextMeshProUGUI startText;
+    private bool gameStarted = false;
+    public string GameSceneName = "Game";
+
+    void Start()
+    {
+        if (IsHost)
+        {
+            startText.gameObject.SetActive(true);
+            startText.text = "Appuyez sur K pour lancer la partie";
+        }
+        else
+        {
+            startText.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (!IsHost || gameStarted)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("L'hôte lance la partie !");
+            gameStarted = true;
+            startText.gameObject.SetActive(false);
+
+            StartGameServerRpc();
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        if (!IsServer)
+        {
+            GetComponentInParent<Canvas>().gameObject.SetActive(false); // plus utile
+            return;
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void StartGameServerRpc(ServerRpcParams rpcParams = default)
+    {
+        StartGameClientRpc();
+    }
+
+    [ClientRpc]
+    private void StartGameClientRpc(ClientRpcParams rpcParams = default)
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("La partie démarre !");
+        if (IsServer)
+        {
+            NetworkManager.SceneManager.LoadScene(GameSceneName, UnityEngine.SceneManagement.LoadSceneMode.Single);
+        }
+    }
+}
